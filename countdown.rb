d03a6431c5ee2e97dad944810af4a56d0334fb41
gncurran <gnc115@gmail.com>
2015-12-08 15:01:36 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 def countdown(n)
-while n >= 1
-  puts "#{n} SECOND(S)!"
-  n -= 1
-end
-"HAPPY NEW YEAR!"
+  while n >= 1
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
 end 
 
 def countdown_with_sleep(n)
